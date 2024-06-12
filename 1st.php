ff4ed00d955bfb1925f36c9df40999008c80fe5d
Rozan99 <rojanghimire99@gmail.com>
2024-06-12 08:11:42 +0545

    Improvised 1st task

--- 1st.php
+++ 1st.php
@@ -1,5 +1,11 @@
 <?php
-echo "I am Rojan";
+echo "I am Rojan<br>";
 echo 5+5;
+echo "<br>";
+$name="Rojan";
+$money="200000";
 
+echo "The employee name is $name and he earns $money as a incentives ";
 ?>
+<!-- variables are case sensitive
+But not others -->
